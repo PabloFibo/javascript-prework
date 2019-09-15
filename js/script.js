e6d4b8f32e9fd438af807bd2df4133ed109e474c
Paweł Hoła <phola85@wp.pl>
2019-09-15 11:40:40 +0200

    Add getMoveName and displayResult functions

--- js/script.js
+++ js/script.js
@@ -2,85 +2,50 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let argComputerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
+console.log('Ruch komputera: ' + argComputerMove);
+
+let argMoveId = randomNumber;
+
+function getMoveName(argMoveId) {
+  if(argMoveId == 1){
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'nożyce';
+  } else if (argMoveId == 3) {
+    return 'papier';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
 }
 
-else if (randomNumber == 2) {
-  computerMove = "nożyce"
-}
-
-else if (randomNumber == 3) {
-  computerMove = "papier"
-}
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Mój ruch to: ' + argComputerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let argPlayerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+console.log('Ruch gracza: ' + argPlayerMove);
+
+printMessage('Twój ruch to: ' + argPlayerMove);
+
+function displayResult(argComputerMove, argPlayerMove){
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  if(argComputerMove == 'kamień' && argPlayerMove == 'papier' ){printMessage('Ty wygrywasz!');
+  }else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){printMessage('Tym razem przegrywasz:(');
+  }else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){printMessage('Remis');
+  }else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {printMessage('Ty wygrywasz');
+  }else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {printMessage('Remis');
+  }else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {printMessage('Tym razem przegrywasz:(');
+  }else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {printMessage('Tym razem przegrywasz:(');
+  }else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {printMessage('Ty wygrywasz');
+  }else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {printMessage('Remis');}
 }
 
-else if (playerInput == '2') {
-  playerMove = 'papier'
-}
+let endResult = displayResult(argComputerMove, argPlayerMove);
 
-else if (playerInput == '3') {
-  playerMove = 'nożyce'
-}
-
-printMessage('Twój ruch to: ' + playerMove);
-
-if (computerMove == 'kamień' && playerMove == 'papier') {
-  printMessage ('Ty wygrywasz')
-}
-
-else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-  printMessage ('Przegrałeś, wygrywa komputer')
-}
-
-else if (computerMove == 'kamień' && playerMove == 'kamień') {
-  printMessage ('Remis, nikt nie wygrywa')
-}
-
-else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-  printMessage ('Remis, nikt nie wygrywa')
-}
-
-else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-  printMessage ('Ty wygrywasz')
-}
-
-else if (computerMove == 'nożyce' && playerMove == 'papier') {
-  printMessage ('Przegrałeś, wygrywa komputer')
-}
-
-else if (computerMove == 'papier' && playerMove == 'nożyce') {
-  printMessage ('Ty wygrywasz')
-}
-
-else if (computerMove == 'papier' && playerMove == 'papier') {
-  printMessage ('Remis, nikt nie wygrywa')
-}
-
-else if (computerMove == 'papier' && playerMove == 'kamień') {
-  printMessage ('Przegrałeś, wygrywa komputer')
-}
-
-else if (computerMove == 'papier' && playerMove == 'nieznany ruch') {
-  printMessage ('Podano nieprawidłową wartość, spróbuj ponownie')
-}
-
-else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
-  printMessage ('Podano nieprawidłową wartość, spróbuj ponownie')
-}
-
-else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
-  printMessage ('Podano nieprawidłową wartość, spróbuj ponownie')
-}
+console.log('Ruch komputer to ' + argComputerMove + ' ,a ruch gracza to ' + argPlayerMove);
